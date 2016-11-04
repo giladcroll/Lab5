c93ed27bbc3489ee2a68a3e5ef5400234c9c5c2e
giladcroll <giladcroll@utexas.edu>
2016-11-04 12:25:58 -0500

    added step and makecritter buttons

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -1,13 +1,20 @@
 package assignment5;
+import java.io.File;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import javax.swing.Painter;
-
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -16,48 +23,123 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 public class Main extends Application { 
 	
 	static GridPane grid = new GridPane();
-	//static GridPane grid2 = new GridPane();
 
 
 @Override public void start(Stage primaryStage) {
 	try {
+		final GridPane stageGrid = new GridPane();
 	    primaryStage.setTitle("Critters!");
-		grid.setGridLinesVisible(false);
-		grid.setHgap(10); //horizontal gap in pixels => that's what you are asking for
-		grid.setVgap(10); //vertical gap in pixels
+	    stageGrid.setGridLinesVisible(false);
+	    stageGrid.setHgap(10); //horizontal gap in pixels => that's what you are asking for
+	    stageGrid.setVgap(10); //vertical gap in pixels
 		// Paints the icons. 
 		//Painter.paint();
-		final FlowPane flow_buttons = new FlowPane();
+		final VBox buttons = new VBox();
 		final FlowPane flow_stats = new FlowPane();
-		final Canvas canvas = new Canvas(assignment5.Params.world_width,assignment5.Params.world_height);
-		final GridPane canvas_grid = new GridPane();
-
-		// button layout
-	    flow_buttons.setMaxWidth(140);
-		flow_buttons.setHgap(10);
-		flow_buttons.setVgap(10);
-		Button btn = new Button();
-	    Button btn1 = new Button();
-	    flow_buttons.getChildren().add(btn);
-	    flow_buttons.getChildren().add(btn1);
-	    btn.setText("Add new Critter");
-	    btn1.setText("yooooo");
-	    btn.setOnAction(new EventHandler<ActionEvent>() {
+		final int worldSize = 500;
+		final Canvas canvas = new Canvas(worldSize,worldSize/2);
+	
+	    // Buttons
+		// makeCritter
+		ArrayList<String> critters = returnCritters();
+		ComboBox critterList = new ComboBox(FXCollections.observableArrayList(critters));
+		critterList.setPromptText("Select Critter");
+		TextField numOfCrt = new TextField ();
+		numOfCrt.setPromptText("number of Critters");
+		// force the field to be numeric only
+		numOfCrt.textProperty().addListener(new ChangeListener<String>() {
 	        @Override
-	        public void handle(ActionEvent event) {
-	            System.out.println("You have created a new Crit!");
+	        public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+	            if (!newValue.matches("\\d*")) {
+	            	numOfCrt.setText(newValue.replaceAll("[^\\d]", ""));
+	            }
 	        }
 	    });
-
+		Label errLbl = new Label("");
+		errLbl.setTextFill(Color.RED);
+		Button makeCrt= new Button("Make Critter");
+		makeCrt.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
+		makeCrt.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	if((critterList.getValue() != null) && !(numOfCrt.getText().equals("")) ){
+            		errLbl.setText("");
+            		for (int i=0; i< Integer.valueOf(numOfCrt.getText()); i++)
+            			try {
+            				Critter.makeCritter((String)critterList.getValue());
+            			} catch (InvalidCritterException e) {}
+            		// clear selection in critter number and in critter selection
+            		numOfCrt.clear();
+            		critterList.getSelectionModel().clearSelection();
+            		Critter.displayWorld();
+            	}
+            	else{
+            		errLbl.setText("Make sure to select a Critter and enter a number");
+            	}
+            }
+        });
+		// make critter Button layout
+		FlowPane flowMakeCrit = new FlowPane((Orientation.HORIZONTAL));
+		flowMakeCrit.getChildren().add(makeCrt);
+        flowMakeCrit.getChildren().add(critterList);
+        flowMakeCrit.getChildren().add(numOfCrt);
+		
+		//Step Button 
+        TextField numOfSteps = new TextField ();
+		numOfSteps.setPromptText("number of steps");
+		// force the field to be numeric only
+			numOfSteps.textProperty().addListener(new ChangeListener<String>() {
+			        @Override
+			        public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+			            if (!newValue.matches("\\d*")) {
+			            	numOfSteps.setText(newValue.replaceAll("[^\\d]", ""));
+			            }
+			        }
+			    });
+		
+		Button stepBtn = new Button("step");
+		stepBtn.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
+		stepBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	if(!(numOfSteps.getText().equals("")) ){
+            		errLbl.setText("");
+            		for (int i=0; i< Integer.valueOf(numOfSteps.getText()); i++)
+            			try {
+            				Critter.worldTimeStep();
+            			} catch (Exception e) {}
+            		// clear selection in step number
+            		numOfSteps.clear();
+            	}
+            	else{	// if no selection step one
+            		Critter.worldTimeStep();
+            	}
+            	Critter.displayWorld(); //display world after stepping
+            }
+        });
+		
+		// step buttons layout
+		FlowPane flowStep = new FlowPane((Orientation.HORIZONTAL));
+		flowStep.getChildren().add(stepBtn);
+		flowStep.getChildren().add(numOfSteps);
+	
+	    // add buttons into container
+	    buttons.getChildren().add(flowMakeCrit);
+	    buttons.getChildren().add(errLbl);
+	    buttons.getChildren().add(flowStep);
+	    
 	    //canvas layout
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		gc.setFill(Color.BLUE);
-		gc.fillRect(0,0,assignment5.Params.world_width,assignment5.Params.world_height);
+		gc.fillRect(0,0,worldSize,worldSize/2);
 		
 	    //stats layout
 	    Label label = new Label("helloooooooooooooooooooooooooooooooo00000000000000000000000000000000000000000000");
@@ -66,28 +148,20 @@ public class Main extends Application {
 	    label.setWrapText(true);
 	    flow_stats.getChildren().add(label);
 
-	   
 		// adding subcontainers into top grid container
-	    grid.add(flow_buttons, 0, 0 ,1, 1);  // col index, row index, col span, row span
-	    grid.add(canvas, 1, 0 ,1, 1);
-	    grid.add(flow_stats, 0, 1 ,2, 1);
+	    stageGrid.add(buttons, 0, 0 ,1, 1);  // col index, row index, col span, row span
+	    stageGrid.add(canvas, 1, 0 ,1, 1);
+	    stageGrid.add(flow_stats, 0, 1 ,2, 1);
 
 	    // show the good stuff
-		Scene scene = new Scene(grid, assignment5.Params.world_width + 200, assignment5.Params.world_height + 200);
+		Scene scene = new Scene(stageGrid, worldSize + 500, worldSize/2 + 200);
 	    primaryStage.setScene(scene);
 	    primaryStage.show();
-	    
 	} catch(Exception e) { e.printStackTrace(); }
 }
 	
 
 public static void main(String[] args) {
-	
-	ArrayList<String> critters = returnCritters();
-	    
-	
-	
-	System.out.println();
 	launch(args);
 }
 
