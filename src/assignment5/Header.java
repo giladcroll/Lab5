8d4297d8c97a18267942338e2946ecf14ff6c4fc
giladcroll <giladcroll@utexas.edu>
2016-11-08 16:21:43 -0600

    Update Header.java

--- src/assignment5/Header.java
+++ src/assignment5/Header.java
@@ -1,12 +1,11 @@
-/* CRITTERS GUI <MyClass.java>
- * EE422C Project 4b submission by
- * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
- * <Student2 Name>
- * <Student2 EID>
- * <Student2 5-digit Unique No.>
+/* CRITTERS Painter.java
+ * EE422C Project 5 submission by
+ * Gilad Croll
+ * gc24654
+ * 16445
+ * Alejandro Stevenson-Duran
+ * as72948
+ * 16455
  * Slip days used: <0>
  * Fall 2016
  */
