f7e3e5c129ffa7b9d6c7e3e110c1705ac0b69cde
AlejandroStevensonDuran <astevensond@utexas.edu>
2016-11-08 16:15:48 -0600

    Update AlgaephobicCritter.java

--- src/assignment5/AlgaephobicCritter.java
+++ src/assignment5/AlgaephobicCritter.java
@@ -1,3 +1,13 @@
+/* EE422C Project 5 submission by
+ * Gilad Croll
+ * gc24654
+ * 16445
+ * Alejandro Stevenson-Duran
+ * as72948
+ * 16455
+ * Slip days used: <0>
+ * Fall 2016
+ */
 package assignment5;
 
 //import project5.Critter.CritterShape;
